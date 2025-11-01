393a47477f4596efa01217d7de65ec42cd48c3c5
sstonn <sstonn2501@gmail.com>
2025-11-01 19:17:09 +0700

    Initial commit

---
+++ Makefile
@@ -0,0 +1,19 @@
+createdb:
+	docker exec -it simple-bank-db createdb --username=root --owner=root simple-bank
+
+dropdb:
+	docker exec -it simple-bank-db dropdb simple-bank
+
+migrateup:
+	migrate -path db/migration -database "postgresql://root:postgres@localhost:5432/simple-bank?sslmode=disable" -verbose up
+
+migratedown:
+	migrate -path db/migration -database "postgresql://root:postgres@localhost:5432/simple-bank?sslmode=disable" -verbose down
+
+sqlc:
+	sqlc generate
+
+test: 
+	go test -v -cover ./...
+
+.PHONY: createdb dropdb migrateup migratedown sqlc
---
+++ util/random.go
@@ -0,0 +1,40 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "VND"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
